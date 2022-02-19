2bc282e965bd35dece86659c8f2cdfa1104c0306
4kcinemas <98001401+4kcinemas@users.noreply.github.com>
2022-02-19 22:05:18 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'Join Our Channel 🎬', url="https://t.me/SS_Linkz"
                                 )
                         ]
                     ]
@@ -47,10 +47,10 @@ async def start(bot, update):
         return
 
     buttons = [[
-        InlineKeyboardButton('Developers', url='https://t.me/CrazyBotsz'),
-        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2')
+        InlineKeyboardButton('Developers', url='https://t.me/SS_Linkz'),
+        InlineKeyboardButton('Source Code 🧾', url ='https://t.me/NetflixMovies_sslinkz')
     ],[
-        InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
+        InlineKeyboardButton('Support 🛠', url='https://t.me/NetflixMovies_sslinkz')
     ],[
         InlineKeyboardButton('Help ⚙', callback_data="help")
     ]]
