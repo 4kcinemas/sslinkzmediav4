4e427743e011f48ab520a5c989b863782f3b0afe
4kcinemas <98001401+4kcinemas@users.noreply.github.com>
2022-02-21 13:24:37 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -54,5 +54,5 @@ For More Details Click Help Button Below..
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://https://t.me/SS_Linkz">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a> href="https://https://t.me/SS_Linkz"Click Me</a></i>
 """
