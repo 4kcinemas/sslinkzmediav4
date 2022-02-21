38280a8074a16dd8ee82f542a10199f8837eb9af
4kcinemas <98001401+4kcinemas@users.noreply.github.com>
2022-02-21 13:47:32 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -184,13 +184,13 @@ async def auto_filter(bot: Client, update: Message):
                 if ((len(ibuttons)%2) == 0):
                     ibuttons.append(
                         [
-                            InlineKeyboardButton(f"⚜ {chat_name} ⚜", url=invite_link)
+                            InlineKeyboardButton(f"⚜ Join Channel ⚜", url=https://t.me/ss_linkz)
                         ]
                     )
 
                 else:
                     ibuttons[-1].append(
-                        InlineKeyboardButton(f"⚜ {chat_name} ⚜", url=invite_link)
+                        InlineKeyboardButton(f"⚜ Join Channel ⚜", url=https://t.me/ss_linkz)
                     )
                 
             for x in ibuttons:
